cfebd7a061444560223ede269a9964d284de56bc
Isabelle Phan <nliphan@gmail.com>
2022-02-26 19:10:20 -0800

    Use snake case for mysql

--- mysql/init/initialize_db.sql
+++ mysql/init/initialize_db.sql
@@ -1,10 +1,10 @@
 USE qbio;
 
 CREATE TABLE metrics (
-  metricId INT(11) NOT NULL AUTO_INCREMENT,
+  metric_id INT(11) NOT NULL AUTO_INCREMENT,
   name VARCHAR(100) NOT NULL,
   formula VARCHAR(100) NOT NULL,
-  PRIMARY KEY (metricId),
+  PRIMARY KEY (metric_id),
   UNIQUE KEY metrics_index1 (name)
 ) 
 ENGINE=INNODB DEFAULT CHARSET=UTF8
@@ -18,29 +18,29 @@ INSERT INTO metrics VALUES
  
 
 CREATE TABLE measurements (
-  measurementId BIGINT(20) NOT NULL AUTO_INCREMENT,
-  metricId INT(11) NOT NULL,
-  timestamp TIMESTAMP NOT NULL,
+  measurement_id BIGINT(20) NOT NULL AUTO_INCREMENT,
+  metric_id INT(11) NOT NULL,
+  `timestamp` TIMESTAMP NOT NULL,
   value DOUBLE,
 
-  PRIMARY KEY (measurementId),
-  KEY measurements_index1 (metricId, timestamp)
+  PRIMARY KEY (measurement_id),
+  KEY measurements_index1 (metric_id, timestamp)
 ) 
 ENGINE=INNODB DEFAULT CHARSET=UTF8
 COMMENT 'Raw measurements';
 
 CREATE TABLE tiered_measurements (
-  tieredMeasurementId BIGINT(20) NOT NULL AUTO_INCREMENT,
+  tiered_measurements_id BIGINT(20) NOT NULL AUTO_INCREMENT,
   tier SMALLINT NOT NULL COMMENT '1: minute level, 2: hour level',
-  metricId INT(11) NOT NULL,
-  timestamp TIMESTAMP NOT NULL,
+  metric_id INT(11) NOT NULL,
+  `timestamp` TIMESTAMP NOT NULL,
   sum DOUBLE NULL COMMENT 'Sum of the raw values',
   min DOUBLE NULL COMMENT 'Min of the raw values',
   max DOUBLE NULL COMMENT 'Max of the raw values',
   sampleSize BIGINT(20) NULL COMMENT 'Number of raw samples',
 
-  PRIMARY KEY (tieredMeasurementId),
-  UNIQUE KEY tiered_measurements_index1 (tier, metricId, timestamp)
+  PRIMARY KEY (tiered_measurements_id),
+  UNIQUE KEY tiered_measurements_index1 (tier, metric_id, `timestamp`)
 ) 
 ENGINE=INNODB DEFAULT CHARSET=UTF8
 COMMENT 'Measurements aggregated at different tiers';
