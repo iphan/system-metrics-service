32dd65076ac1bb77518969e34b04c2da5a5d39be
Isabelle Phan <nliphan@gmail.com>
2022-02-27 16:27:50 -0800

    fix initial db schema

--- mysql/init/initialize_db.sql
+++ mysql/init/initialize_db.sql
@@ -12,7 +12,7 @@ COMMENT 'Metric metadata';
 
 INSERT INTO metrics VALUES
 (1, "cpu-usage-percent", "[CPU]Totl%"),
--- (2, "memory-usage-percent", "[MEM]Used / [MEM]Tot"),
+(2, "memory-usage-percent", "[MEM]Used / [MEM]Tot"),
 (3, "network-kb-in", "[NET]RxKBTot"),
 (4, "network-kb-out", "[NET]TxKBTot");
  
@@ -24,7 +24,7 @@ CREATE TABLE measurements (
   value DOUBLE,
 
   PRIMARY KEY (measurement_id),
-  KEY measurements_index1 (metric_id, timestamp)
+  KEY measurements_index1 (metric_id, `timestamp`)
 ) 
 ENGINE=INNODB DEFAULT CHARSET=UTF8
 COMMENT 'Raw measurements';
@@ -33,14 +33,16 @@ CREATE TABLE tiered_measurements (
   tiered_measurements_id BIGINT(20) NOT NULL AUTO_INCREMENT,
   tier SMALLINT NOT NULL COMMENT '1: minute level, 2: hour level',
   metric_id INT(11) NOT NULL,
-  `timestamp` TIMESTAMP NOT NULL,
+  `timestamp` TIMESTAMP NOT NULL COMMENT 'End of time window',
   sum DOUBLE NULL COMMENT 'Sum of the raw values',
   min DOUBLE NULL COMMENT 'Min of the raw values',
   max DOUBLE NULL COMMENT 'Max of the raw values',
-  sampleSize BIGINT(20) NULL COMMENT 'Number of raw samples',
+  sample_size BIGINT(20) NULL COMMENT 'Number of raw samples',
 
   PRIMARY KEY (tiered_measurements_id),
   UNIQUE KEY tiered_measurements_index1 (tier, metric_id, `timestamp`)
 ) 
 ENGINE=INNODB DEFAULT CHARSET=UTF8
 COMMENT 'Measurements aggregated at different tiers';
+
+
